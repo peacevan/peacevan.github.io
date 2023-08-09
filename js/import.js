a6449d73a955519e4f6f3bced40e8b27d8357bc5
IVAN AMADO CARDOSO <peacevan@hotmail.com>
2023-08-09 10:43:03 -0300

    adicionado dado portfolio

---
+++ js/import.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    setTimeout(function(){
+      $("#footer").load("footer.html");
+    },0);
+    setTimeout(function(){
+      $("#nav").load("nav.html");
+    },0);
+});
